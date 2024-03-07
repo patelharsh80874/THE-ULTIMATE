c874220b03d46bf4ec526af4dde8150d66d30c73
HARSH PATEL <patelharsh80874@yahoo.com>
2024-03-07 17:33:04 +0530

    fix

--- src/components/templates/PlyerTwo.jsx
+++ src/components/templates/PlyerTwo.jsx
@@ -27,7 +27,7 @@ const PlyerTwo = () => {
         /> */}
   
         <iframe
-        sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
+        sandboxscrolling="no" sandbox="allow-scripts allow-same-origin"
         allowFullScreen
           className="w-[90vw] sm:h-[80vh] h-[80vh] "
           src={`https://embed.smashystream.com/playere.php?tmdb=${ytvideo}`}
